2c3ef89c93eb26613f91d1bd043925987f48a9eb
Denis Smirnov <gonzru@gmail.com>
2022-09-17 23:28:15 +0300

    lesson 8: classNames

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@ import {Link, Route, Routes} from "react-router-dom";
 import {AboutPageAsync} from "./pages/About/AboutPage.async";
 import {MainPageAsync} from "./pages/Main/MainPage.async";
 import {useTheme} from "./theme/useTheme";
+import {classNames} from "./helpers/classNames";
 
 const App = () => {
 
     const {theme, toggleTheme} = useTheme();
 
     return (
-        <div className={`app ${theme}`}>
+        <div className={classNames('app', {}, [theme])}>
             <button onClick={toggleTheme}>TOGGLE THEME</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О нас</Link>
