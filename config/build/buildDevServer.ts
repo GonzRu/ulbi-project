4f127dc879a3ad48c228c74eb4935626ba61036b
Denis Smirnov <gonzru@gmail.com>
2022-09-17 22:15:23 +0300

    lessong 6: router + lazy loading

--- config/build/buildDevServer.ts
+++ config/build/buildDevServer.ts
@@ -4,5 +4,6 @@ import {Configuration as DevServerConfiguration} from 'webpack-dev-server'
 export function buildDevServer(options: BuildOptions): DevServerConfiguration {
     return {
         port: options.port,
+        historyApiFallback: true,
     }
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-import {Counter} from "../components/Counter";
+import {Suspense} from 'react';
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+import {AboutPageAsync} from "../pages/About/AboutPage.async";
+import {MainPageAsync} from "../pages/Main/MainPage.async";
 
 const App = () => {
     return (
         <div className='app'>
-            dfsdfsdf
-            <Counter/>
+            <Link to={'/'}>Главная</Link>
+            <Link to={'/about'}>О нас</Link>
+            <Suspense fallback={<div>Загрузка...</div>}>
+                <Routes>
+                    <Route path={'/about'} element={<AboutPageAsync/>}/>
+                    <Route path={'/'} element={<MainPageAsync/>}/>
+                </Routes>
+            </Suspense>
         </div>
     );
 };
